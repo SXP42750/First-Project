62ee6cd888ae93e0db489d7caa8f3c0f4f0ae0b3
saika <sxp42750@ucmo.edu>
2024-08-18 15:03:00 -0400

    Check the number is Prime or not

--- venv/Prime.py
+++ venv/Prime.py
@@ -1,1 +1,13 @@
+n = int(input("Enter the number:"))  # input number
 
+if n <= 1:
+    print("Not prime")
+else:
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            print("Not prime")
+            break
+        i += 1
+    else:
+        print("Prime")
